e3e9ff45ba776832165f5d7fe84eb8af0ebf4825
Quentin Pradelle <45031699+quentinprad@users.noreply.github.com>
2018-11-30 22:05:39 +0100

    Add files via upload
    Update Models and Main with some parameter tuning options

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from models import *
 test = pd.read_csv('/Users/quentinpradelle/Desktop/EssecCentrale/Cours Centrale/Machine Learning/AssignmentsAndProject/ML-DSBA-AI-Assignment_2/Data/test.csv')
 train = pd.read_csv('/Users/quentinpradelle/Desktop/EssecCentrale/Cours Centrale/Machine Learning/AssignmentsAndProject/ML-DSBA-AI-Assignment_2/Data/train.csv')
 full = train.append(test, ignore_index = True)
+X_labels = train['Survived']
+X_labels = X_labels.values
 
 print('Dataset \'full\' shape:', full.shape)
 
@@ -34,10 +36,6 @@ full['Embarked'].value_counts()
 # Transform sex column to 0s and 1s
 full['Sex'] = full['Sex'].map({'male' : 0, 'female' : 1}).astype(int)
 
-# Create a column Family size and remove Parch and SibSp
-full['FamilySize'] = full['Parch'] + full['SibSp'] + 1
-full.drop(['Parch', 'SibSp'], axis=1, inplace=True)
-
 # Get only first letter for Cabin Code ands replace Null values by 'U'
 full['CabinCode'] = full[full['Cabin'].notnull()].Cabin.astype(str).str[0]
 full['CabinCode'].replace(NaN, 'U', inplace=True)
@@ -56,10 +54,23 @@ SurvivalbyTitle = full.groupby('Title')
 
 for key, item in SurvivalbyTitle:
     print(SurvivalbyTitle.get_group(key), "\n\n")
+    
+# Create the column child, and indicate whether child or adult
+full['Child'] = (full['Age'] < 18) * 1
+
+# Creating the column Mother 
+full['Mother'] = ((full.Sex == 1) & (full.Parch > 0) & (full.Title != 1)) * 1
+
+# Create a column Family size and remove Parch and SibSp
+full['FamilySize'] = full['Parch'] + full['SibSp'] + 1
+full.drop(['Parch', 'SibSp'], axis=1, inplace=True)
 
 # Transform missing ages with the mean of similar passengers
 full['Age'] = full.groupby(['Pclass', 'Title'])['Age'].transform(lambda x: x.fillna(x.median()))
-full['Age'].median()
+full['Age'].fillna((full['Age'].median()), inplace=True)
+
+# Create age * class feature
+full['age_times_class']=full['Age']*full['Pclass']
 
 # Transform Cabin Codes with numbers
 full['CabinCode'].value_counts()
@@ -68,29 +79,52 @@ full['CabinCode'] = full['CabinCode'].map({'U' : 0, 'C' : 1, 'B' : 2, 'D' : 3, '
 # Transform Embarked column with numbers
 full['Embarked'].value_counts()
 full['Embarked'] = full['Embarked'].map({'S' : 0, 'C' : 1, 'Q' : 2}).astype(int)
+full = pd.get_dummies(full, prefix = 'dummy_Embarked', columns = ['Embarked'], drop_first = True)
+
+# Transform Title column to dummy
+full = pd.get_dummies(full, prefix = 'dummy_Title', columns = ['Title'], drop_first = True)
 
 # Remove the unneccessary column 
 full.drop(['Cabin', 'FamilyName', 'Name', 'Ticket', 'PassengerId'], axis=1, inplace=True)
+
+full['AgeBin'] = pd.cut(full['Age'].astype(int), 4, labels=[0, 1, 2, 3])
+full['AgeBin'] = full['AgeBin'].astype(int)
+
+# Check if there are nan values remaining / 418 are normal (test set survived column), 1 is not normal 
+full.isnull().values.any()
+full.isnull().sum().sum()
+
+# Save the cleaned dataframe as a csv file
+full.to_csv("dfTitanicCleanFull.csv", sep = ',')
 
 # Separate test and train set from the cleaned full set
 full = full.convert_objects(convert_numeric = True)
 full = full.values 
 
+# Save the cleaned numpy array as a csv file
+savetxt("titanicCleanFull.csv", full, delimiter=",")
+
+
+# Convert nan value in age to median value
+#full[979, 0] = median(full, axis = 1)[0]
+
 X_train = full[:891, :]
 X_test = full[891:, :]
-Y_train = X_train[:, 5] # Survived column
+Y_train = X_train[:, 4] # Survived column
 
-X_train = delete(X_train, 5, 1)
-X_test = delete(X_test, 5, 1)
+X_train = delete(X_train, 4, 1)
+X_test = delete(X_test, 4, 1)
 
 # After analysis, we discovered that the four most important features were
 
-X_train = delete(X_train, 1, 1)
-X_test = delete(X_test, 1, 1)
+#X_train = delete(X_train, 8, 1)
+#X_test = delete(X_test, 8, 1)
 
-X_train = delete(X_train, 5, 1)
-X_test = delete(X_test, 5, 1)
-
+#X_train = delete(X_train, 1, 1)
+#X_test = delete(X_test, 1, 1)
+#
+#X_train = delete(X_train, 5, 1)
+#X_test = delete(X_test, 5, 1)
 
 #________________________________________________________________________________________________________________
 
@@ -113,10 +147,22 @@ for trainIndex, testIndex in kf:
     for i in range(testSet.shape[0]):
         if predictedLabels[i] == testLabels[i]:
             correct += 1
-        
+    
     print 'Accuracy: ' + str(float(correct)/(testLabels.size))
     totalCorrect += correct
     totalInstances += testLabels.size
 print 'Total Accuracy: ' + str(totalCorrect/float(totalInstances))
-	
-RandomForestFeatures(trainSet, trainLabels, testSet)
+
+# Now, let's do the prediction for the Kaggle test set
+
+Y_predict = zeros((418, 2))
+for i in range(0, 418):
+    Y_predict[i, 0] = 892 + i
+    Y_train = Y_train.astype(float)
+Y_predict[:, 1] = RandomForest(X_train, Y_train, X_test)
+
+Y_predict = Y_predict.astype(int)
+
+final_result = pd.DataFrame({'PassengerId': Y_predict[:, 0], 'Survived': Y_predict[:, 1]})
+
+final_result.to_csv('submission.csv', index = False)
--- models.py
+++ models.py
@@ -6,254 +6,198 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import make_classification
+import matplotlib.pyplot as plt
 from sklearn.datasets import make_classification
 from sklearn.ensemble import ExtraTreesClassifier
-from sklearn import ensemble
-from sklearn import datasets
-import matplotlib.pyplot as plt
-from sklearn import linear_model
-from sklearn.svm import l1_min_c
-from sklearn.naive_bayes import GaussianNB
-from sklearn.pipeline import Pipeline
-from sklearn.feature_selection import SelectFromModel
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import LinearSVC 
+from sklearn.svm import SVC  
+from sklearn.datasets import make_hastie_10_2
+from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import *
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.feature_selection import SelectFromModel
 
+def ScaleData(Set):
 
+   # This function will normalize the data through z = (data - mean) / std
+   scaler = StandardScaler()
+   scaler.fit(Set)
 
-
-def Scaledata(Set):
-    
-    scaler = StandardScaler()
-    scaler.fit(Set)
-    
-    return Set
+   return Set
 
 
 def DecisionTree(trainSet, trainLabels, testSet):
 	
     # Train a single decision tree
-    clf = DecisionTreeClassifier(max_depth=7)
-
+    clf = DecisionTreeClassifier()
     # Train the classifier
     clf.fit(trainSet, trainLabels)
-
     # Do classification on the test dataset and return 
     predictedLabels = clf.predict(testSet)
-
+    
     return predictedLabels
 
 def DecisionTreeWithFeatureSelection(trainSet, trainLabels, testSet):
-    
-   # Train a single decision tree
-    clf = DecisionTreeClassifier(max_depth=7)
 
-   # Create a selector object that will use the random forest classifier to identify
-    # features that have an importance of more than 0.05
-    sfm = SelectFromModel(clf, threshold=0.04)
-    # Train the selector
-    sfm.fit(trainSet, trainLabels)
-    trainSet_trans = sfm.transform(trainSet)
-    testSet_trans = sfm.transform(testSet)
-    clf.fit(trainSet_trans, trainLabels)
-    predictedLabels = clf.predict(testSet_trans)
+  # Train a single decision tree
+   clf = DecisionTreeClassifier(max_depth=7)
+
+  # Create a selector object that will use the random forest classifier to identify
+   # features that have an importance of more than 0.05
+   sfm = SelectFromModel(clf, threshold=0.05)
+   
+   # Train the selector
+   sfm.fit(trainSet, trainLabels)
+   trainSet_trans = sfm.transform(trainSet)
+   testSet_trans = sfm.transform(testSet)
+   clf.fit(trainSet_trans, trainLabels)
+   predictedLabels = clf.predict(testSet_trans)
+
+   return predictedLabels
+
+def RandomForest(trainSet, trainLabels, testSet):
+    
+    clf = RandomForestClassifier(n_estimators = 50, max_features = 'log2', bootstrap = False, max_depth = 10, min_samples_leaf = 4, random_state = 0)
+    clf.fit(trainSet, trainLabels)
+
+    predictedLabels = clf.predict(testSet)
     
     return predictedLabels
+
+def RandomForestFeatures(trainSet, trainLabels, testSet):
+
+    # Build a forest and compute the feature importances
+    forest = ExtraTreesClassifier(n_estimators = 250, random_state=0)
+
+    # Fit the model and sort the features by importance
+    forest.fit(trainSet, trainLabels)
+    importances = forest.feature_importances_
+    stddev = std([tree.feature_importances_ for tree in forest.estimators_],
+             axis=0)
+    indices = argsort(importances)[::-1]
+
+    # Print the features ranking
+    print("Feature ranking:")
+
+    for f in range(trainSet.shape[1]):
+        print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
+
+    # Plot the feature importances of the forest
+    plt.figure()
+    plt.title("Feature importances")
+    plt.bar(range(trainSet.shape[1]), importances[indices], color="r", yerr=stddev[indices], align="center")
+    plt.xticks(range(trainSet.shape[1]), indices)
+    plt.xlim([-1, trainSet.shape[1]])
+    plt.show()
+
+
+def RandomForestWithFeatureSelection(trainSet, trainLabels, testSet):
+
+   clf = RandomForestClassifier(n_estimators = 450, max_features = 'log2', bootstrap = False, max_depth = 75, min_samples_leaf = 4)
+   # Create a selector object that will use the random forest classifier to identify
+   
+   # features that have an importance of more than 0.15
+   sfm = SelectFromModel(clf, threshold=0.02)
+   
+   # Train the selector
+   sfm.fit(trainSet, trainLabels)
+   
+   trainSet_trans = sfm.transform(trainSet)
+   testSet_trans = sfm.transform(testSet)
+   
+   clf.fit(trainSet_trans, trainLabels)
+   
+   predictedLabels = clf.predict(testSet_trans)
+
+   return predictedLabels
 
 
 def LogReg(trainSet, trainLabels, testSet):
 
     # Train 
-    clf = LogisticRegression(penalty='l1',n_jobs =-1,solver='liblinear',C=1)
+    clf = LogisticRegression(C = 1e12, penalty = 'l2', random_state= 33)
     clf.fit(trainSet,trainLabels)
-
     predictedLabels = clf.predict(testSet)
 
     return predictedLabels
 
 
+def Knn(trainSet, trainLabels, testSet):
     
-
-def lda(trainSet, trainLabels, testSet):
-    
-    clf = LinearDiscriminantAnalysis()
+    clf = KNeighborsClassifier(n_neighbors = 10)
     clf.fit(trainSet, trainLabels)
-    
     predictedLabels = clf.predict(testSet)
     
     return predictedLabels
 
-def knn(trainSet, trainLabels, testSet):
+def SVM(trainSet, trainLabels, testSet):
     
-    clf = KNeighborsClassifier()
-    
+    clf = SVC() 
     clf.fit(trainSet, trainLabels)
-    
+    supportVectors = clf.support_vectors_
     predictedLabels = clf.predict(testSet)
     
     return predictedLabels
 
+def Boosting(trainSet, trainLabels, testSet):
     
-def RandomForest(trainSet, trainLabels, testSet):
-    
-    clf = RandomForestClassifier(n_estimators=50, max_depth=10, random_state=0)
+    clf = GradientBoostingClassifier(n_estimators=119, learning_rate=1.0, max_depth=1, random_state=0)
     clf.fit(trainSet, trainLabels)
-    print("FEATURE IMPORTANCE:")
-    print(clf.feature_importances_) 
     predictedLabels = clf.predict(testSet)
     
     return predictedLabels
 
 
-    
-   
-def RandomForestFeatures(trainSet, trainLabels, testSet):
-
-
-
-    # Build a forest and compute the feature importances
-
-    forest = ExtraTreesClassifier(n_estimators=250,
-
-                              random_state=0)
-
-
-
-    forest.fit(trainSet, trainLabels)
-
-    importances = forest.feature_importances_
-
-    stddev = std([tree.feature_importances_ for tree in forest.estimators_],
-
-             axis=0)
-
-    indices = argsort(importances)[::-1]
-
-
-
-    # Print the feature ranking
-
-    print("Feature ranking:")
-
-
-
-    for f in range(trainSet.shape[1]):
-
-        print("%d. feature %d (%f)" % (f + 1, indices[f], importances[indices[f]]))
-
-
-
-
-
-    # Plot the feature importances of the forest
-
-    plt.figure()
-
-    plt.title("Feature importances")
-
-    plt.bar(range(trainSet.shape[1]), importances[indices], color="r", yerr=stddev[indices], align="center")
-
-    plt.xticks(range(trainSet.shape[1]), indices)
-
-    plt.xlim([-1, trainSet.shape[1]])
-
-    plt.show()
-    
-
-def RandomForestWithFeatureSelection(trainSet, trainLabels, testSet):
-
-    clf = RandomForestClassifier(class_weight=None, max_depth=10, max_features='auto', n_estimators=400)
-    # Create a selector object that will use the random forest classifier to identify
-    # features that have an importance of more than 0.15
-    sfm = SelectFromModel(clf, threshold=0.05)
-    # Train the selector
-    sfm.fit(trainSet, trainLabels)
-    trainSet_trans = sfm.transform(trainSet)
-    testSet_trans = sfm.transform(testSet)
-    clf.fit(trainSet_trans, trainLabels)
-    predictedLabels = clf.predict(testSet_trans)
-    
-    return predictedLabels
-
-    
-    
-    
-    
-def GaussNaiveBayes(trainSet, trainLabels, testSet):
-    
-    gnb = GaussianNB()
-    gnb.fit(trainSet, trainLabels)
-    predictedLabels = gnb.predict(testSet)
-    return predictedLabels
-
-    
-
-def AverageModels(trainSet, trainLabels, testSet):
-    
-    randfor = RandomForest(trainSet, trainLabels, testSet)
-    gauss = gaussNaiveBayes(trainSet, trainLabels, testSet)
-    loreg = logReg(trainSet, trainLabels, testSet)
-    ldaa = lda(trainSet, trainLabels, testSet)
-    
-    average_variable = (randfor+loreg+ldaa+randfor) / 4.0
-    for i in range(len(average_variable)):
-        if average_variable[i] < 0.5:
-            average_variable[i] = 0
-        else:
-            average_variable[i] = 1
-    return average_variable
-
-        
-    
-#def test_rf(trainSet, trainLabels, testSet):
-#    
-#    clf = Pipeline([
-#            ('feature_selection', SelectFromModel(LinearSVC(penalty="l1"))),
-#            ('classification', RandomForestClassifier())
-#                                                        ])
-#    clf.fit(trainSet, trainLabels)
-#    predictedLabels = clf.predict(testSet)
-#    
-#    return predictedLabels
-#    
-    
-
-
-def Tuning(trainSet, trainLabels):
-   import numpy as np
+def tuning(trainSet, trainLabels, testSet):
+    import numpy as np
 # Number of trees in random forest
-   n_estimators = [int(x) for x in np.linspace(start = 350, stop = 450, num = 10)]
+    n_estimators = [int(x) for x in np.linspace(start = 150, stop = 450, num = 10)]
 # Number of features to consider at every split
-   max_features = ['auto']
+    max_features = ['auto', 'log2']
 # Maximum number of levels in tree
-   max_depth = [int(x) for x in np.linspace(10, 110, num = 21)]
-   max_depth.append(None)
+    max_depth = [int(x) for x in np.linspace(10, 110, num = 21)]
+    max_depth.append(None)
 # Minimum number of samples required to split a node
-   # 'min_samples_split': min_samples_split,min_samples_split = 0.5
-
+    # 'min_samples_split': min_samples_split,min_samples_split = 0.5
+    
 # Minimum number of samples required at each leaf node
-   min_samples_leaf = [1, 2, 4]
+    min_samples_leaf = [1, 2, 4]
 # Method of selecting samples for training each tree
-   bootstrap = [True, False]
+    bootstrap = [True, False]
 # Create the random grid
-   random_grid = {'n_estimators': n_estimators,
-              'max_features': max_features,
-              'max_depth': max_depth,
-              'min_samples_leaf': min_samples_leaf,
-              'bootstrap': bootstrap}
+    random_grid = {'n_estimators': n_estimators,
+               'max_features': max_features,
+               'max_depth': max_depth,
+               'min_samples_leaf': min_samples_leaf,
+               'bootstrap': bootstrap}
 
 # Use the random grid to search for best hyperparameters
 # First create the base model to tune
-   rf = RandomForestClassifier()
+    rf = RandomForestRegressor()
 # Random search of parameters, using 3 fold cross validation, 
 # search across 100 different combinations, and use all available cores
-   rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 5, verbose=2, random_state=42, n_jobs = -1)
+    rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 2, verbose=2, random_state=42, n_jobs = -1)
 # Fit the random search model
-   rf_random.fit(trainSet, trainLabels)
-
-   print('best params = ', rf_random.best_params_)
-
-   return rf_random.best_params_ 
+    rf_random.fit(trainSet, trainLabels)
     
+    print('best params = ', rf_random.best_params_)
+
+    return rf_random.best_params_
+
+    
+def averageModels(trainSet, trainLabels, testSet):
+    
+    logreg = LogReg(trainSet, trainLabels, testSet)
+    decisiontree = DecisionTree(trainSet, trainLabels, testSet)
+    randomforest = RandomForest(trainSet, trainLabels, testSet)
+    svma = SVM(trainSet, trainLabels, testSet)
+    Boost = Boosting(trainSet, trainLabels, testSet)
+        
+    averageModels = (Boost + logreg + randomforest + decisiontree + randomforest) / 5.0
+    for i in range(len(averageModels)):
+        if averageModels[i] < 0.5:
+            averageModels[i] = 0
+        else:
+            averageModels[i] = 1
+    
+    return averageModels
     
